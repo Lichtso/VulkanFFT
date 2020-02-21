ca773ec16f8230ed8f192cc332106cd3612228ca
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-02-21 23:19:33 +0100

    Fixes travis build and adds deployment

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,34 +3,38 @@
 export VK_VERSION=1.2.131.2
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install libpng openexr
-    curl -GO https://sdk.lunarg.com/sdk/download/$VK_VERSION/mac/vulkan-sdk.tar.gz
+    curl -Go vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/mac/vulkan-sdk.tar.gz?human=true
     tar zxf vulkan-sdk.tar.gz
     export VULKAN_SDK=$TRAVIS_BUILD_DIR/vulkansdk-macos-$VK_VERSION/macOS
-cat > environments.sh << EOF
+    sudo cp -R $VULKAN_SDK/Frameworks/* /Library/Frameworks/
+    cat > environments.sh << EOF
 export VULKAN_SDK=$VULKAN_SDK
 export VK_TOOLS=$VULKAN_SDK/bin
 export VK_ICD_FILENAMES=$VULKAN_SDK/etc/vulkan/icd.d/MoltenVK_icd.json
 export VK_LAYER_PATH=$VULKAN_SDK/etc/vulkan/explicit_layer.d
 EOF
-    sudo cp -R $VULKAN_SDK/Frameworks/* /Library/Frameworks/
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get -qq update
     sudo apt-get install -y cmake libpng16-dev libopenexr-dev
-    curl -GO https://sdk.lunarg.com/sdk/download/$VK_VERSION/linux/vulkan-sdk.tar.gz
+    curl -Go vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/linux/vulkan-sdk.tar.gz?human=true
     tar zxf vulkan-sdk.tar.gz
     export VULKAN_SDK=$TRAVIS_BUILD_DIR/$VK_VERSION/x86_64
-cat > environments.sh << EOF
+    sudo cp -R $VULKAN_SDK/include/* /usr/local/include/
+    sudo cp -R $VULKAN_SDK/lib/* /usr/local/lib/
+    cat > environments.sh << EOF
 export VULKAN_SDK=$VULKAN_SDK
 export VK_TOOLS=$VULKAN_SDK/bin
 export VK_ICD_FILENAMES=~/dev/mesa/share/vulkan/icd.d/intel_icd.x86_64.json
 export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
 EOF
-    sudo cp -R $VULKAN_SDK/include/* /usr/local/include/
-    sudo cp -R $VULKAN_SDK/lib/* /usr/local/lib/
 elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
-    curl -GO https://sdk.lunarg.com/sdk/download/$VK_VERSION/windows/vulkan-sdk.exe
-    start /wait vulkan-sdk.exe /S
-type > environments.sh << EOF
-export VULKAN_SDK=C:\VulkanSDK\$VK_VERSION
+    curl -Go vulkan-sdk.exe https://sdk.lunarg.com/sdk/download/$VK_VERSION/windows/vulkan-sdk.exe?human=true
+    7z x vulkan-sdk.exe
+    export VULKAN_SDK=$TRAVIS_BUILD_DIR
+    setx -m VULKAN_SDK $VULKAN_SDK
+    type > environments.sh << EOF
+export VK_TOOLS=$VULKAN_SDK/Bin
+export VK_ICD_FILENAMES=/windows/system32/nv-vk64.json
+export VK_LAYER_PATH=$VULKAN_SDK/Bin
 EOF
 fi
