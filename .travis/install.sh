447490f7a4a9024cb38154e9fd84c6c344cccbb8
Alexander Meißner <AlexanderMeissner@gmx.net>
2020-07-09 05:58:30 +0200

    Adds possible redirection to curl download in CI install script.

--- .travis/install.sh
+++ .travis/install.sh
@@ -3,7 +3,7 @@
 export VK_VERSION=1.2.141.2
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew install libpng openexr
-    curl -Go vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/mac/vulkan-sdk.tar.gz?human=true
+    curl -LGo vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/mac/vulkan-sdk.tar.gz?human=true
     tar zxf vulkan-sdk.tar.gz
     export VULKAN_SDK=$TRAVIS_BUILD_DIR/vulkansdk-macos-$VK_VERSION/macOS
     sudo cp -R $VULKAN_SDK/Frameworks/* /Library/Frameworks/
@@ -16,7 +16,7 @@ EOF
 elif [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo apt-get -qq update
     sudo apt-get install -y cmake libpng16-dev libopenexr-dev
-    curl -Go vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/linux/vulkan-sdk.tar.gz?human=true
+    curl -LGo vulkan-sdk.tar.gz https://sdk.lunarg.com/sdk/download/$VK_VERSION/linux/vulkan-sdk.tar.gz?human=true
     tar zxf vulkan-sdk.tar.gz
     export VULKAN_SDK=$TRAVIS_BUILD_DIR/$VK_VERSION/x86_64
     sudo cp -R $VULKAN_SDK/include/* /usr/local/include/
@@ -28,7 +28,7 @@ export VK_ICD_FILENAMES=~/dev/mesa/share/vulkan/icd.d/intel_icd.x86_64.json
 export VK_LAYER_PATH=$VULKAN_SDK/etc/explicit_layer.d
 EOF
 elif [[ $TRAVIS_OS_NAME == 'windows' ]]; then
-    curl -Go vulkan-sdk.exe https://sdk.lunarg.com/sdk/download/$VK_VERSION/windows/vulkan-sdk.exe?human=true
+    curl -LGo vulkan-sdk.exe https://sdk.lunarg.com/sdk/download/$VK_VERSION/windows/vulkan-sdk.exe?human=true
     7z x vulkan-sdk.exe
     export VULKAN_SDK=$TRAVIS_BUILD_DIR
     cat > environments.sh << EOF
